0ebdf47f98ea1f7771de109c3832b494f2fd5349
SrDark04 <rogermao5.1@gmail.com>
2026-09-25 00:33:53 -0500

    Feature: test and enroute urls, with configs in app.jsx class

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,18 @@ import BlueprintsPage from './pages/BlueprintsPage.jsx'
 import BlueprintDetailPage from './pages/BlueprintDetailPage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
 import NotFound from './pages/NotFound.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx'
+import BlueprintForm from './components/BlueprintForm.jsx'
+import { useDispatch } from 'react-redux'
+import { createBlueprint } from './features/blueprints/blueprintsSlice.js'
 
 export default function App() {
+
+  const dispatch = useDispatch()
+  const handleCreate = (formData) => {
+    dispatch(createBlueprint(formData));
+  }
+
   return (
     <div className="container">
       <header>
@@ -16,10 +26,16 @@ export default function App() {
           <NavLink to="/login">Login</NavLink>
         </nav>
       </header>
+
       <Routes>
         <Route path="/" element={<BlueprintsPage />} />
         <Route path="/blueprints/:author/:name" element={<BlueprintDetailPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/new" element={
+          <PrivateRoute>
+            <BlueprintForm onSubmit={handleCreate} />
+          </PrivateRoute>
+        } />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
